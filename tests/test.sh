629ae5f482f3dd5ae533e33a8654c53b1172ee93
Ben Vaughan <111064685+bvaughan-nexient@users.noreply.github.com>
2024-02-08 10:08:08 -0600

    library template updates and test chart (#1)
    * library template updates and test chart
    
    Added a test chart to consume the library templates.
    * includes test script for local debugging
    * includes new github workflow for testing on PR
    
    Modified library templates to be more feature complete for our anticpated use cases.
    
    * fix typo
    
    * debug
    
    * debug
    
    * more looping debug
    
    * typo

---
+++ tests/test.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# This is a simple test script to test the functionality of the helm chart
+OUTFILE=$(mktemp -d --tmpdir=.)/test-manifest.yaml
+KUBEVERSION="1.26.5"
+
+helm dependencies update
+helm template . > $OUTFILE
+if [ $? -eq 0 ]; then
+  echo "Helm chart is able to generate the manifest files successfully"
+else
+  echo "Helm chart build failed"
+  exit 1
+fi
+
+helm lint . --debug
+if [ $? -eq 0 ]; then
+  echo "Helm chart is linting successfully"
+else
+  echo "Helm chart linting failed"
+  exit 1
+fi
+
+kubeconform -strict -summary -kubernetes-version $KUBEVERSION -exit-on-error $OUTFILE
+if [ $? -eq 0 ]; then
+  echo "Manifest file is valid"
+  if [ -f $OUTFILE ]; then
+    rm -f $OUTFILE
+    rmdir $(dirname $OUTFILE)
+  fi
+else
+  echo "Manifest file is invalid"
+  exit 1
+fi
